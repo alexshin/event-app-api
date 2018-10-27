bef2b13aca432f75f92bb66def8719350e35e788
Alex Shinkevich <a.shinkevich@ittopgroup.com>
2018-10-27 12:04:12 +0300

    Added api documentation to prod environment

--- src/_app/envs/prod/settings.py
+++ src/_app/envs/prod/settings.py
@@ -1,4 +1,48 @@
+from os import getenv
 from ..shared import *
 
 
 DEBUG = False
+
+BASE_URL = getenv('APP_BASE_URL', 'http://example.com')
+
+ALLOWED_HOSTS = list(str(getenv('APP_ALLOWED_HOSTS', '127.0.0.1')).split(','))
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = getenv('APP_SECRET_KEY', 'something difficult to brute force')
+
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.postgresql',  # This app uses postgres-json fields, it works only with PGSql
+        'NAME':     getenv('POSTGRES_DB', 'app'),
+        'USER':     getenv('POSTGRES_USER', 'localroot'),
+        'PASSWORD': getenv('POSTGRES_PASSWORD', 'localrootpass'),
+        'HOST':     getenv('POSTGRES_HOST', 'postgres'),
+        'PORT':     getenv('POSTGRES_PORT', '5432'),
+    }
+}
+
+# Caches
+REDIS_HOST = getenv('REDIS_HOST', 'redis')
+REDIS_PORT = getenv('REDIS_PORT', '6379')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': f'redis://{REDIS_HOST}:{REDIS_PORT}/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [
+                (REDIS_HOST, REDIS_PORT)
+            ],
+        },
+    },
+}
--- src/_app/envs/prod/urls.py
+++ src/_app/envs/prod/urls.py
@@ -1,1 +1,18 @@
 from ..shared.urls import APPLICATION_URLS
+from ..dev.urls import schema_view
+
+from django.urls import re_path
+
+
+APPLICATION_URLS += [
+    re_path(
+        r'^api/doc/swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=None),
+        name='schema-json'
+    ),
+    re_path(
+        r'^api/doc/$',
+        schema_view.with_ui('swagger', cache_timeout=None),
+        name='schema-swagger-ui'
+    )
+]
