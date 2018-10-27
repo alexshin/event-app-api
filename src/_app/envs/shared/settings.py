e64d80fb233bcf6f53406a1b5e6c1abd235ab430
Alex Shinkevich <a.shinkevich@ittopgroup.com>
2018-10-27 16:54:29 +0300

    Added static support

--- src/_app/envs/shared/settings.py
+++ src/_app/envs/shared/settings.py
@@ -144,3 +144,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100
 }
+
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..', 'static'))
