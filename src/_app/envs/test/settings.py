2d30bd851bec33c869e51f8223f51494a920eb83
Alex Shinkevich <alex.shinkevich@gmail.com>
2019-04-11 13:51:32 +0300

    Added travis ci

--- src/_app/envs/test/settings.py
+++ src/_app/envs/test/settings.py
@@ -1,3 +1,4 @@
+from os import getenv
 from ..shared import *
 
 DEBUG = False
@@ -9,11 +10,11 @@ SECRET_KEY = 'b=c(untxoz5s!9sudc9u!)b%(w=029(0d2pzodl04m(3x35e=l'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'app',
-        'USER': 'localroot',
-        'PASSWORD': 'localrootpass',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME':     getenv('DJANGO_TEST_DB_NAME', 'app'),
+        'USER':     getenv('DJANGO_TEST_DB_USER', 'localroot'),
+        'PASSWORD': getenv('DJANGO_TEST_DB_PASS', 'localrootpass'),
+        'HOST':     getenv('DJANGO_TEST_DB_HOST', '127.0.0.1'),
+        'PORT':     getenv('DJANGO_TEST_DB_PORT', '5432'),
     }
 }
 
